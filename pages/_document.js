492137cca17e463830b687feec0263c5c182e3a5
Adwirawien <hannsadrian@gmail.com>
2019-09-28 18:13:26 +0200

    PWA Updates

--- pages/_document.js
+++ pages/_document.js
@@ -25,6 +25,7 @@ export default class CustomDocument extends Document {
             sizes="16x16"
             href="/favicon-16x16.png"
           />
+          <link rel="manifest" href="/manifest.json" />
           <link rel="manifest" href="/site.webmanifest" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <link rel="stylesheet" href="/background.css" />
