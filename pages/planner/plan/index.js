0594b2a0ba0dd71b614a4b25f97a186a55f383e5
Adwirawien <hannsadrian@gmail.com>
2019-10-06 01:04:49 +0200

    Update route planner

--- pages/planner/plan/index.js
+++ pages/planner/plan/index.js
@@ -4,6 +4,14 @@ import Head from "next/head";
 import Link from "next/link";
 import "../../../static/tailwind.css";
 import { BarLoader } from "react-spinners";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faExchangeAlt,
+  faClock,
+  faArrowLeft
+} from "@fortawesome/free-solid-svg-icons";
+var moment = require("moment");
+require("moment-duration-format");
 
 class Index extends React.Component {
   constructor(props) {
@@ -31,7 +39,6 @@ class Index extends React.Component {
     departure.setUTCHours(time[0]);
     departure.setUTCMinutes(time[1]);
     departure.setUTCFullYear(date[2], date[1] - 1, date[0]);
-    console.log(departure);
 
     var route = await dvb
       .route(origin[0].id, destination[0].id, departure)
@@ -54,20 +61,8 @@ class Index extends React.Component {
         <Head>
           <title>Public Transport Planner</title>
         </Head>
-        <h1 className="font-semibold font-sans text-3xl text-gray-200 leading-tight">
-          Public Transport Planner
-        </h1>
 
-        {this.state.err === "" &&
-        this.state.loading === false &&
-        this.state.route !== {} &&
-        this.state.route.origin ? (
-          <p className="font-sans text-gray-500 leading-tight mb-5">
-            {this.state.route.origin.name +
-              " - " +
-              this.state.route.destination.name}
-          </p>
-        ) : this.state.err === "" ? (
+        {this.state.err === "" && this.state.loading ? (
           <div className="rounded overflow-hidden max-w-xs pb-2 pt-3">
             <BarLoader
               heightUnit={"px"}
@@ -78,50 +73,119 @@ class Index extends React.Component {
               loading={true}
             />
           </div>
-        ) : (
+        ) : this.state.err !== "" ? (
           <p className="p-1 pl-2 bg-red-600 text-gray-300 mt-4 mb-5 max-w-xs rounded font-semibold">
             {this.state.err}
           </p>
+        ) : (
+          <></>
         )}
         {this.state.err === "" &&
         this.state.route.trips &&
         this.state.route.trips.length > 0 ? (
           <div>
             {this.state.route.trips.map((trip, index) => (
-              <div className="p-1 pl-2 pt-3 bg-unselected text-gray-300 mt-4 max-w-xs rounded">
+              <div className="p-1 pl-4 pr-4 pt-4 bg-gray-900 text-gray-300 mt-4 max-w-sm rounded">
                 {trip.departure !== undefined ? (
-                  <h3 className="ml-1 font-semibold">
-                    <span className="text-gray-500 font-light">
+                  <p className="leading-tight font-semibold truncate mt-2">
+                    <span className="uppercase font-mono tracking-wide font-bold text-sm text-gray-600">
+                      {trip.departure.platform.type +
+                        " " +
+                        trip.departure.platform.name}
+                    </span>{" "}
+                    <br></br>
+                    {trip.departure.name + ", " + trip.departure.city} <br></br>
+                    <span className="tracking-tighter text-gray-500 font-light font-mono">
                       {String(trip.departure.time.getHours()).padStart(2, "0") +
                         ":" +
                         String(trip.departure.time.getMinutes()).padStart(
                           2,
                           "0"
-                        )}
-                    </span>{" "}
-                    {trip.departure.name + ", " + trip.departure.city}
-                  </h3>
+                        ) +
+                        " " +
+                        String(trip.departure.time.getDate()).padStart(2, "0") +
+                        "." +
+                        String(trip.departure.time.getMonth() + 1).padStart(
+                          2,
+                          "0"
+                        ) +
+                        "." +
+                        String(trip.departure.time.getFullYear())}
+                    </span>
+                  </p>
                 ) : (
                   <></>
                 )}
+                <div className="my-3 ml-2 font-mono">
+                  <p>
+                    <FontAwesomeIcon
+                      icon={faExchangeAlt}
+                      className="mr-3 h-4"
+                    ></FontAwesomeIcon>
+                    <span className="my-auto">
+                      {trip.interchanges}{" "}
+                      {trip.interchanges === 1 ? "interchange" : "interchanges"}
+                    </span>
+                  </p>
+                  <p>
+                    <FontAwesomeIcon
+                      icon={faClock}
+                      className="mr-3 h-4"
+                    ></FontAwesomeIcon>
+                    <span className="my-auto">
+                      {trip.duration < 60
+                        ? moment
+                            .duration(trip.duration, "minutes")
+                            .format("m") +
+                          (moment
+                            .duration(trip.duration, "minutes")
+                            .format("m") === 1
+                            ? " minute"
+                            : " minutes")
+                        : moment
+                            .duration(trip.duration, "minutes")
+                            .format("h") +
+                          (moment
+                            .duration(trip.duration, "minutes")
+                            .format("h") === 1
+                            ? " hours"
+                            : " hour")}
+                    </span>
+                  </p>
+                </div>
                 {trip.arrival !== undefined ? (
                   <>
-                    <h3 className="ml-1 font-semibold">
-                      <span className="text-gray-500 font-light">
+                    <p className="leading-tight font-semibold truncate">
+                      <span className="uppercase font-mono tracking-wide font-bold text-sm text-gray-600">
+                        {trip.arrival.platform.type +
+                          " " +
+                          trip.arrival.platform.name}
+                      </span>{" "}
+                      <br></br>
+                      {trip.arrival.name + ", " + trip.arrival.city} <br></br>
+                      <span className="tracking-tighter text-gray-500 font-light font-mono">
                         {String(trip.arrival.time.getHours()).padStart(2, "0") +
                           ":" +
                           String(trip.arrival.time.getMinutes()).padStart(
                             2,
                             "0"
-                          )}
-                      </span>{" "}
-                      {trip.arrival.name + ", " + trip.arrival.city}
-                    </h3>
-                    <hr className="mt-2 mb-3 ml-1 mr-3 border-gray-700"></hr>
+                          ) +
+                          " " +
+                          String(trip.arrival.time.getDate()).padStart(2, "0") +
+                          "." +
+                          String(trip.arrival.time.getMonth() + 1).padStart(
+                            2,
+                            "0"
+                          ) +
+                          "." +
+                          String(trip.arrival.time.getFullYear())}
+                      </span>
+                    </p>
                   </>
                 ) : (
                   <></>
                 )}
+                <hr className="mt-6 mb-3 border-gray-700"></hr>
                 {trip.nodes.map((node, index) => (
                   <div className="mb-5">
                     <div className="flex justify-between my-2">
@@ -132,8 +196,7 @@ class Index extends React.Component {
                               ? { display: "hidden", marginRight: "0" }
                               : {
                                   height: "24px",
-                                  marginRight: "0.5rem",
-                                  marginLeft: "0.5rem"
+                                  marginRight: "0.5rem"
                                 }
                           }
                           className="my-auto w-auto"
@@ -142,6 +205,8 @@ class Index extends React.Component {
                             node.line.includes("U") &&
                             node.mode.title.includes("undefined")
                               ? "https://upload.wikimedia.org/wikipedia/commons/a/a3/U-Bahn.svg"
+                              : node.mode.title.includes("taxi")
+                              ? "https://www.dvb.de/assets/img/trans-icon/transport-alita.svg"
                               : node.mode.icon_url
                           }
                           onError={() => {
@@ -166,7 +231,7 @@ class Index extends React.Component {
                           {node.departure.name + ", " + node.departure.city}
                           {node.stops.length > 0 &&
                           typeof node.stops[0].platform !== "undefined" ? (
-                            <p className="uppercase tracking-wide font-bold text-xs text-gray-600">
+                            <p className="uppercase font-mono tracking-wide font-bold text-sm text-gray-600">
                               {String(node.departure.time.getHours()).padStart(
                                 2,
                                 "0"
@@ -189,7 +254,7 @@ class Index extends React.Component {
                           {node.stops.length > 0 &&
                           typeof node.stops[node.stops.length - 1].platform !==
                             "undefined" ? (
-                            <p className="uppercase tracking-wide font-bold text-xs text-gray-600">
+                            <p className="uppercase font-mono tracking-wide font-bold text-sm text-gray-600">
                               {String(node.arrival.time.getHours()).padStart(
                                 2,
                                 "0"
