4630ee53c4fd9d0bda9274021600a71f7b1ec9fe
Adwirawien <hannsadrian@gmail.com>
2019-10-06 01:04:29 +0200

    Fix layout issues for certain browsers

--- pages/planner/index.js
+++ pages/planner/index.js
@@ -146,36 +146,38 @@ class Index extends React.Component {
                 <FontAwesomeIcon icon={faArrowLeft}></FontAwesomeIcon>
               </button>
             </Link>
-            <Cleave
-              placeholder={
-                String(new Date().getHours()).padStart(2, "0") +
-                ":" +
-                String(new Date().getMinutes()).padStart(2, "0")
-              }
-              options={{
-                time: true,
-                delimiter: ":",
-                timePattern: ["h", "m"]
-              }}
-              onChange={this.onTimeChange.bind(this)}
-              className="hover:bg-black w-3/12 text-lg font-sans font-semibold trans rounded-l px-3 py-2 bg-gray-900 text-gray-200 placeholder-gray-400 focus:outline-none"
-            />
-            <Cleave
-              placeholder={
-                String(new Date().getDate()).padStart(2, "0") +
-                "." +
-                String(new Date().getMonth() + 1).padStart(2, "0") +
-                "." +
-                new Date().getFullYear()
-              }
-              options={{
-                date: true,
-                delimiter: ".",
-                datePattern: ["d", "m", "Y"]
-              }}
-              onChange={this.onDateChange.bind(this)}
-              className="hover:bg-black w-5/12 text-lg font-sans font-semibold trans rounded-r px-3 py-2 bg-gray-900 text-gray-200 placeholder-gray-400 focus:outline-none mr-3"
-            />
+            <div className="flex rounded overflow-hidden w-auto mr-3">
+              <Cleave
+                placeholder={
+                  String(new Date().getHours()).padStart(2, "0") +
+                  ":" +
+                  String(new Date().getMinutes()).padStart(2, "0")
+                }
+                options={{
+                  time: true,
+                  delimiter: ":",
+                  timePattern: ["h", "m"]
+                }}
+                onChange={this.onTimeChange.bind(this)}
+                className="hover:bg-black w-2/5 text-lg font-sans font-semibold trans px-3 py-2 bg-gray-900 text-gray-200 placeholder-gray-400 focus:outline-none"
+              />
+              <Cleave
+                placeholder={
+                  String(new Date().getDate()).padStart(2, "0") +
+                  "." +
+                  String(new Date().getMonth() + 1).padStart(2, "0") +
+                  "." +
+                  new Date().getFullYear()
+                }
+                options={{
+                  date: true,
+                  delimiter: ".",
+                  datePattern: ["d", "m", "Y"]
+                }}
+                onChange={this.onDateChange.bind(this)}
+                className="hover:bg-black w-3/5 text-lg font-sans font-semibold trans px-3 py-2 bg-gray-900 text-gray-200 placeholder-gray-400 focus:outline-none"
+              />
+            </div>
             <button
               onClick={this.redirect}
               disabled={!this.checkValid()}
@@ -187,20 +189,22 @@ class Index extends React.Component {
               <FontAwesomeIcon icon={faSearch}></FontAwesomeIcon>
             </button>
           </div>
-          <input
-            placeholder="from"
-            id="from"
-            value={this.state.from}
-            onChange={this.handleChange}
-            className="hover:bg-black min-w-full text-lg font-sans font-semibold trans rounded-t px-3 py-2 bg-gray-900 text-gray-200 placeholder-gray-500 focus:outline-none"
-          ></input>
-          <input
-            placeholder="to"
-            id="to"
-            value={this.state.to}
-            onChange={this.handleChange}
-            className="hover:bg-black mb-3 min-w-full text-lg font-sans font-semibold trans rounded-b px-3 py-2 bg-gray-900 text-gray-200 placeholder-gray-500 focus:outline-none"
-          ></input>
+          <div className="rounded overflow-hidden mb-3">
+            <input
+              placeholder="from"
+              id="from"
+              value={this.state.from}
+              onChange={this.handleChange}
+              className="hover:bg-black min-w-full text-lg font-sans font-semibold trans px-3 py-2 bg-gray-900 text-gray-200 placeholder-gray-500 focus:outline-none"
+            ></input>
+            <input
+              placeholder="to"
+              id="to"
+              value={this.state.to}
+              onChange={this.handleChange}
+              className="hover:bg-black min-w-full text-lg font-sans font-semibold trans px-3 py-2 bg-gray-900 text-gray-200 placeholder-gray-500 focus:outline-none"
+            ></input>
+          </div>
           <div className="w-full bg-gray-900 text-gray-200 font-semibold font-sans rounded bg-gray-900">
             {this.state.suggestions.map((value, index) => {
               return (
